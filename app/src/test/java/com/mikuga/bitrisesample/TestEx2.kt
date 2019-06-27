3e012ddd7351fd0c6df0873ab82fb2012d0d15a2
Anber2 <anber.sub@gmail.com>
2019-06-27 11:55:24 +0900

    new test33

---
+++ app/src/test/java/com/mikuga/bitrisesample/TestEx2.kt
@@ -0,0 +1,12 @@
+package com.mikuga.bitrisesample
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class TestEx2 {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
